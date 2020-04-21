f8338b0ea32a927362cf86548a21b08908065aaf
Irina <maria.irina_gherman@stud.acs.upb.ro>
2020-04-22 00:35:03 +0300

    small maybe bugfix, might solve our promote problems :?

--- src/com/company/Board/BoardCommands.java
+++ src/com/company/Board/BoardCommands.java
@@ -315,6 +315,7 @@ public class BoardCommands {
 
 			Bitboard source = getBitboardFromType(getPieceType(sourceIndex));
 			source.clearBit(sourceIndex);
+			board.updateBitboards();
 			return 1;
 		}
 
